f5cd43e96ff1e25865db82ca0f55f96a24d1eff1
Futago-za Ryuu <futagoza.ryuu@gmail.com>
2019-02-18 08:31:06 +0000

    Add docs with Vuepress support

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,68 @@
+"use strict";
+
+module.exports = {
+
+    title: "POW",
+    description: "Run spec, benchmark and impact tests all from one tool",
+    head: [
+
+        [ "meta", { "http-equiv": "XA-UA-Compatible", content: "IE=edge" } ],
+        [ "link", { rel: "shortcut icon", href: "/favicon.ico", type: "image/x-icon" } ],
+
+    ],
+    dest: "dist",
+    themeConfig: {
+
+        nav: [
+            {
+                text: "Documentation",
+                items: [
+                    {
+                        text: "Guides",
+                        link: "/guides/getting-started",
+                    },
+                    {
+                        text: "API",
+                        link: "/guides/javascript-api",
+                    },
+                    {
+                        text: "Compatibility",
+                        link: "/guides/compatibility",
+                    },
+                ],
+            },
+            {
+                text: "Development",
+                items: [
+                    {
+                        text: "Contribute",
+                        link: "/development",
+                    },
+                    {
+                        text: "Changelog",
+                        link: "/changelog",
+                    },
+                    {
+                        text: "Source code",
+                        link: "https://github.com/pegjs/pegjs",
+                    },
+                    {
+                        text: "Issue tracker",
+                        link: "https://github.com/pegjs/pegjs/issues",
+                    },
+                ],
+            },
+        ],
+        sidebar: false,
+        // sidebarDepth: 0,
+        // displayAllHeaders: false,
+        // activeHeaderLinks: true,
+        search: true,
+        searchMaxSuggestions: 5,
+        lastUpdated: false,
+        editLinks: false,
+        evergreen: false,
+
+    },
+
+};
