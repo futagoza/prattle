668c0a2dcee403926ad899434d94bcffd6e24211
Futago-za Ryuu <futagoza.ryuu@gmail.com>
2019-02-19 12:46:14 +0000

    Remove extranous files from old revision

--- src/banner.js
+++
@@ -1,5 +0,0 @@
-/*
-    POW.js, https://github.com/futagoza/pow
-    Copyright © 2016+ Futago-za Ryuu, https://futagoza.github.io/
-    Released under the MIT License, http://opensource.org/licenses/MIT
-*/
