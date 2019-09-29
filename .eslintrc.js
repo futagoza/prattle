6ca36a0728bd909b034cac782dd97fc341391fc3
Futago-za Ryuu <futagoza.ryuu@gmail.com>
2019-09-29 23:20:19 +0100

    Update ESLint configuration
    - Move all local configs to root config
    - Extend from '@futagoza/*' configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,23 @@
 
 module.exports = {
 
-    "extends": "futagozaryuu/node-v4",
+    "extends": [
+
+        "@futagoza/node/4",
+        "@futagoza/vue",
+
+    ],
+
+    "overrides": [ {
+
+        "files": [
+            "*.spec.js",
+            "*.test.js",
+        ],
+        "extends": "@futagoza/dev/test",
+
+    } ],
+
     "root": true,
 
 };
