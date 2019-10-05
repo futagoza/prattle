f6cac322046cf07ae8112baaf5b96b619c789a90
Futago-za Ryuu <futagoza.ryuu@gmail.com>
2019-10-05 22:55:58 +0100

    Use my all-in-one ESLint config only

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,23 +2,7 @@
 
 module.exports = {
 
-    "extends": [
-
-        "@futagoza/node/8",
-        "@futagoza/vue",
-
-    ],
-
-    "overrides": [ {
-
-        "files": [
-            "*.spec.js",
-            "*.test.js",
-        ],
-        "extends": "@futagoza/dev/test",
-
-    } ],
-
+    "extends": "@futagoza",
     "root": true,
 
 };
